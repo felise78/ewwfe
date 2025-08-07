2110c9062d2b34ccad606787fb4bf64010bf3688
Helene MOTTU <hemottu@f2r11s11.clusters.42paris.fr>
2025-08-07 18:19:17 +0200

    added stars but dunnow if i'll keep it this way

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import TextureManager from './TextureManager.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import getStarfield from './getStarfield.js';
 
 // mettre en exposition global pour pouvoir explorer l'API dans la console du navigateur :
 // window.THREE = THREE;
@@ -14,6 +16,10 @@ const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 renderer.setSize(w, h);
 ////////////////////////////////////////////////////////////////////
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * (Math.PI / 180); 
+scene.add(earthGroup);
+
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
@@ -21,17 +27,25 @@ scene.background = new THREE.Color(0x000000);
 
 const geometry = new THREE.IcosahedronGeometry(1, 12);
 
-const loader = new THREE.TextureLoader();
+// const loader = new THREE.TextureLoader();
+
+const textureManager = new TextureManager();
+const mainTexture = await textureManager.loadTexture('./textures/Albedo.jpg');
+
 const material = new THREE.MeshPhongMaterial({
-  map: loader.load("./textures/Albedo.jpg"),
-//   specularMap: loader.load("./textures/02_earthspec1k.jpg"),
-//   bumpMap: loader.load("./textures/01_earthbump1k.jpg"),
+  map: mainTexture,
+  // specularMap: loader.load("./textures/02_earthspec1k.jpg"),
+  // bumpMap: loader.load("./textures/01_earthbump1k.jpg"),
 //   bumpScale: 0.04,
 });
 
 
+
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(earthMesh);
+earthGroup.add(earthMesh);
+
+const stars = getStarfield();
+scene.add(stars);
 
 
 const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
@@ -43,11 +57,10 @@ scene.add( light );
 
 function animate() {
 
-    requestAnimationFrame(animate);
-  
-    earthMesh.rotation.x += 0.001;
-    earthMesh.rotation.y += 0.002; 
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+
+  earthMesh.rotation.y += 0.002; 
+  renderer.render(scene, camera);
 }
 
 animate();
@@ -56,3 +69,4 @@ animate();
 // ajouter des update de controls pour ameliorer l'interaction
 // dat.gui ?
 // rajouter le event listeners pour resize et update camera
+// stats pour les performances
